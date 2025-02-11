854d98b411db6f364012263f91b8693e3a7b9245
Jakub Labath <jakub.labath@dronedeploy.com>
2025-02-11 22:07:39 +0000

    add sample use in client

--- client.clj
+++ client.clj
@@ -3,13 +3,12 @@
 (require
  '[netpod.pods :as pods])
 
-
 ;;before running do
 ;;cargo build --release
 ;;to build the binary
-(pods/with-pod "./target/debug/netpod-jlabath-gcs"
+(pods/with-pod "./target/release/netpod-jlabath-gcs"
   ;; require is not suitable in macros
   ;; but one can also resolve things dynamically using resolve such as below
   (let [meta (resolve 'netpod.jlabath.gcs/meta)]
-    (println @(meta "testerson"))))
+    (println @(meta "gs://some-bucket/somefile.jpg"))))
 
