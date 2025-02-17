572ec6f9cd417dd6d9269705d14caa1253b0e62b
Jakub Labath <jakub@labath.ca>
2025-02-17 02:21:46 +0000

    use the shared library

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use anyhow::{Result, anyhow};
-use std::convert::TryFrom;
-use std::{collections::HashMap, env, fs, path::Path, process, sync::Arc};
-mod netpod;
-use netpod::{invoke_response, run_server, HandlerFn, HandlerFuture, Request, Response};
 use google_cloud_storage::client::{Client, ClientConfig};
 use google_cloud_storage::http::objects::get::GetObjectRequest;
+use netpod_base::{
+    error::{from_error, NetpodError},
+    invoke_response, run_server, HandlerFn, HandlerFuture, Request, Response,
+};
+use std::{
+    collections::HashMap, convert::TryFrom, env, error::Error, fs, path::Path, process,
+    result::Result, sync::Arc,
+};
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         eprintln!("Expected exactly one argument!");
@@ -22,7 +25,7 @@ async fn main() -> Result<()> {
     }
 
     let config = ClientConfig::default().with_auth().await?;
-    let client = Arc::new(Client::new(config)); // Wrap in Arc for shared access
+    let client = Arc::new(Client::new(config));
 
     let meta_handler = create_handler(client.clone(), |res, req| Box::pin(handle_meta(res, req)));
 
@@ -41,36 +44,44 @@ where
     Box::new(move |req: Request| Box::pin(my_handler(shared_resource.clone(), req)))
 }
 
-async fn handle_meta(shared_resource: Arc<Client>, req: Request) -> Result<Response> {
-    let decoded_args: Vec<String> = serde_json::from_str(&req.args.ok_or_else(|| anyhow!("invalid args"))?)?;
-    let gs_path =  decoded_args.first().ok_or(anyhow!("no gs:// filepath given"))?;
+async fn handle_meta(shared_resource: Arc<Client>, req: Request) -> Result<Response, NetpodError> {
+    let args_str = req
+        .args
+        .ok_or_else(|| NetpodError::from("args are missing"))?;
+    let decoded_args: Vec<String> =
+        serde_json::from_str(&args_str).map_err(|e| NetpodError::from(e.to_string()))?;
+    let gs_path = decoded_args
+        .first()
+        .ok_or_else(|| NetpodError::Message("no gs:// filepath given".to_string()))?;
     let o_req = GcsObjectRequest::try_from(gs_path.as_str())?;
-    let object = shared_resource.get_object(&o_req.0).await?;
+    let object = shared_resource
+        .get_object(&o_req.0)
+        .await
+        .map_err(from_error)?;
     //eprintln!("Object: {:?}", &object);
-    let data = serde_json::to_vec(&object)?;
+    let data = serde_json::to_vec(&object).map_err(from_error)?;
     Ok(invoke_response(
-        req.id.unwrap_or_else( || "missing".to_string()),
+        req.id.unwrap_or_else(|| "missing".to_string()),
         data,
     ))
 }
-
 
 /// Wrapper struct to allow `TryFrom<String>`
 #[derive(Debug)]
 struct GcsObjectRequest(GetObjectRequest);
 
 impl TryFrom<&str> for GcsObjectRequest {
-    type Error = anyhow::Error;
+    type Error = NetpodError;
 
     fn try_from(gs_url: &str) -> Result<Self, Self::Error> {
         if !gs_url.starts_with("gs://") {
-            return Err(anyhow!("does not start with gs://"));
+            return Err("does not start with gs://".into());
         }
 
         let path = &gs_url[5..]; // Remove "gs://"
         let parts: Vec<&str> = path.splitn(2, '/').collect();
         if parts.len() < 2 {
-            return Err(anyhow!("expected bucket and object but got {}", path));
+            return Err(format!("expected bucket and object but got {}", path).into());
         }
 
         Ok(GcsObjectRequest(GetObjectRequest {
@@ -80,5 +91,3 @@ impl TryFrom<&str> for GcsObjectRequest {
         }))
     }
 }
-
-
